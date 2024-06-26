80e72792f6af2e5e1879f9fa7c42ff8bc047d59c
omar karake <omar.karake@amalitechtraining.org>
2024-06-26 12:41:56 +0200

    updating range

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const inputRange = document.getElementById("inputRange");
+let charCount = document.getElementById("char-count");
+charCount.innerText = inputRange.value;
+
 const activeColor = "#A4FFAF";
 const inactiveColor = "#18171F";
 
 inputRange.addEventListener("input", function () {
   const ratio = ((this.value - this.min) / (this.max - this.min)) * 100;
   this.style.background = `linear-gradient(90deg, ${activeColor} ${ratio}%, ${inactiveColor} ${ratio}%)`;
+  charCount.innerText = this.value;
 });

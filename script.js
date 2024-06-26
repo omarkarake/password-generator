a0c8d402c693b49dec87f852ba0e8077c44bf77f
omar karake <omar.karake@amalitechtraining.org>
2024-06-26 22:00:41 +0200

    refactored the codes to remove the dry codes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Select DOM elements
 const beforeCopy = document.querySelector(".before-copy");
 const afterCopy = document.querySelector(".after-copy");
 const copied = document.querySelector("#copied");
@@ -10,46 +11,37 @@ const checkedSvg = document.querySelectorAll(".checked");
 const generateBtn = document.querySelector(".generate-btn");
 const generateInside = document.querySelector(".generate");
 const strengthIndicator = document.querySelector(".medium-text");
-const color1 = document.getElementById("strength-color1");
-const color2 = document.getElementById("strength-color2");
-const color3 = document.getElementById("strength-color3");
-const color4 = document.getElementById("strength-color4");
+const colors = [
+  document.getElementById("strength-color1"),
+  document.getElementById("strength-color2"),
+  document.getElementById("strength-color3"),
+  document.getElementById("strength-color4"),
+];
 
 let tobeCopied;
 
+// Event listener for copying the generated password to the clipboard
 icons.addEventListener("click", async () => {
   try {
-    if (tobeCopied === undefined) {
-      generatedPassElement.textContent = "Please generate first...";
-      if (generatedPassElement.textContent.length > 20)
-        generatedPassElement.style.fontSize = "12px";
-      generatedPassElement.style.opacity = ".25";
+    if (!tobeCopied) {
+      setGeneratedPassElementText("Please generate first...");
       return;
     }
     await navigator.clipboard.writeText(tobeCopied);
     // Show the copied message
-    copied.style.visibility = "visible";
-    setTimeout(() => {
-      copied.style.visibility = "hidden";
-    }, 3000);
+    showCopiedMessage();
   } catch (err) {
     console.error("Failed to copy text: ", err);
   }
 });
 
-icons.addEventListener("mouseover", () => {
-  beforeCopy.style.display = "none";
-  afterCopy.style.display = "block";
-});
+// Event listeners for mouseover and mouseout to change icon display
+icons.addEventListener("mouseover", () => toggleCopyIcons(false));
+icons.addEventListener("mouseout", () => toggleCopyIcons(true));
 
-icons.addEventListener("mouseout", () => {
-  beforeCopy.style.display = "block";
-  afterCopy.style.display = "none";
-});
-
+// Update character count display and range input background
 charCount.innerText = rangeInput.value;
 let lengthInput = rangeInput.value;
-let percent = 0;
 
 const activeColor = "#A4FFAF";
 const inactiveColor = "#18171F";
@@ -58,80 +50,45 @@ rangeInput.addEventListener("input", function () {
   const ratio = ((this.value - this.min) / (this.max - this.min)) * 100;
   this.style.background = `linear-gradient(90deg, ${activeColor} ${ratio}%, ${inactiveColor} ${ratio}%)`;
   charCount.innerText = this.value;
-  lengthInput = rangeInput.value;
+  lengthInput = this.value;
 });
 
+// Event listener for checkboxes to update their styles
 checkboxes.forEach((checkbox, index) => {
-  let insideIndex = index;
-  checkbox.addEventListener("click", () => {
-    if (checkbox.hasAttribute("style")) {
-      checkbox.removeAttribute("style");
-      checkedSvg.forEach((svg, index) => {
-        if (index === insideIndex) {
-          svg.classList.remove("show-checked");
-          checkbox.classList.remove("checked-box");
-        }
-      });
-    } else {
-      checkbox.style.backgroundColor = "var(--color-neon-green)";
-      checkbox.style.border = "2px solid var(--color-neon-green)";
-      checkbox.classList.add("checked-box");
-      checkedSvg.forEach((svg, index) => {
-        if (index === insideIndex) {
-          svg.classList.add("show-checked");
-        }
-      });
-    }
-  });
+  checkbox.addEventListener("click", () => toggleCheckbox(checkbox, index));
 });
 
-generateBtn.addEventListener("mouseover", () => {
-  generateInside.innerHTML = `<p>GENERATE</p><img
-                 src="./assets/images/icon-arrow-right-neon-green.svg"
-                 class="icon-right"
-             />`;
-});
+// Event listeners for generate button hover effect
+generateBtn.addEventListener("mouseover", () => updateGenerateButton(true));
+generateBtn.addEventListener("mouseout", () => updateGenerateButton(false));
 
-generateBtn.addEventListener("mouseout", () => {
-  generateInside.innerHTML = `<p>GENERATE</p><img src="./assets/images/icon-arrow-right.svg" class="icon-right"/>`;
-});
+// Character pools for password generation
+const charTypes = {
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "0123456789",
+  symbols: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
+};
 
-const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-
+// Function to generate password based on selected options and length
 function generatePassword() {
   let characterPool = "";
   let password = "";
   const length = parseInt(lengthInput);
+
   if (length <= 0) {
-    generatedPassElement.textContent = "Please increase character length";
-    if (generatedPassElement.textContent.length > 20)
-      generatedPassElement.style.fontSize = "12px";
-    generatedPassElement.style.opacity = ".25";
+    setGeneratedPassElementText("Please increase character length");
     return;
   }
 
-  if (document.querySelector(".checkbox1").classList.contains("checked-box")) {
-    characterPool += uppercaseLetters;
-  }
+  Object.keys(charTypes).forEach((type, i) => {
+    if (checkboxes[i].classList.contains("checked-box")) {
+      characterPool += charTypes[type];
+    }
+  });
 
-  if (document.querySelector(".checkbox2").classList.contains("checked-box")) {
-    characterPool += lowercaseLetters;
-  }
-  if (document.querySelector(".checkbox3").classList.contains("checked-box")) {
-    characterPool += numbers;
-  }
-  if (document.querySelector(".checkbox4").classList.contains("checked-box")) {
-    characterPool += symbols;
-  }
-
-  if (characterPool === "") {
-    generatedPassElement.textContent = "Select at least one option";
-    if (generatedPassElement.textContent.length > 20)
-      generatedPassElement.style.fontSize = "12px";
-    generatedPassElement.style.opacity = ".25";
+  if (!characterPool) {
+    setGeneratedPassElementText("Select at least one option");
     return;
   }
 
@@ -141,82 +98,78 @@ function generatePassword() {
     );
   }
 
-  generatedPassElement.textContent = password;
-  generatedPassElement.style.fontSize = "2rem";
-  generatedPassElement.style.opacity = "1";
-  tobeCopied = generatedPassElement.textContent;
+  setGeneratedPassElementText(password, true);
+  tobeCopied = password;
   updateStrength();
 }
 
+// Function to update the strength indicator based on selected options
 function updateStrength() {
   const selectedOptions = Array.from(checkboxes).filter((checkbox) =>
     checkbox.classList.contains("checked-box")
   ).length;
   const percentage = (selectedOptions / checkboxes.length) * 100;
-  percent = percentage;
-  // Correctly assign the value of the custom property
-  //   if password strength is below 100 and up 75
-  if (percent > 75 && percent >= 100) {
-    color1.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-neon-green");
-    color2.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-neon-green");
-    color3.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-neon-green");
-    color4.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-neon-green");
-    strengthIndicator.innerText = "STRONG";
-    //   if password strength is below 75 and up 50
-  } else if (percent > 50 && percent >= 75) {
-    color1.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-yellow");
-    color2.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-yellow");
-    color3.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-yellow");
-    color4.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("");
-    strengthIndicator.innerText = "MEDIUM";
-    //   if password strength is below 50 and up 25
-  } else if (percent > 25 && percent >= 50) {
-    color1.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-orange");
-    color2.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-orange");
-    color3.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("");
-    color4.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("");
-    strengthIndicator.innerText = "WEAK";
-    //   if password strength is below or equal to 25
-  } else if (percent <= 25) {
-    console.log(color1);
-    color1.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--color-red");
-    color2.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("");
-    color3.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("");
-    color4.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("");
-    strengthIndicator.innerText = "TOO WEAK!";
+
+  let strength = "TOO WEAK!";
+  let colorValue = "--color-red";
+  if (percentage > 75) {
+    strength = "STRONG";
+    colorValue = "--color-neon-green";
+  } else if (percentage > 50) {
+    strength = "MEDIUM";
+    colorValue = "--color-yellow";
+  } else if (percentage > 25) {
+    strength = "WEAK";
+    colorValue = "--color-orange";
+  }
+
+  colors.forEach((color, i) => {
+    color.style.backgroundColor =
+      i < selectedOptions
+        ? getComputedStyle(document.documentElement).getPropertyValue(
+            colorValue
+          )
+        : "";
+  });
+  strengthIndicator.innerText = strength;
+}
+
+// Event listener to generate password when button is clicked
+generateBtn.addEventListener("click", generatePassword);
+
+// Helper functions
+function toggleCopyIcons(showBefore) {
+  beforeCopy.style.display = showBefore ? "block" : "none";
+  afterCopy.style.display = showBefore ? "none" : "block";
+}
+
+function toggleCheckbox(checkbox, index) {
+  const checked = checkbox.classList.toggle("checked-box");
+  checkbox.style.backgroundColor = checked ? "var(--color-neon-green)" : "";
+  checkbox.style.border = checked ? "2px solid var(--color-neon-green)" : "";
+  checkedSvg[index].classList.toggle("show-checked", checked);
+}
+
+function updateGenerateButton(hover) {
+  generateInside.innerHTML = `<p>GENERATE</p><img src="./assets/images/icon-arrow-right${
+    hover ? "-neon-green" : ""
+  }.svg" class="icon-right"/>`;
+}
+
+function setGeneratedPassElementText(text, reset = false) {
+  generatedPassElement.textContent = text;
+  if (reset) {
+    generatedPassElement.style.fontSize = "2rem";
+    generatedPassElement.style.opacity = "1";
+  } else {
+    generatedPassElement.style.fontSize = text.length > 20 ? "12px" : "";
+    generatedPassElement.style.opacity = ".25";
   }
 }
 
-generateBtn.addEventListener("click", generatePassword);
+function showCopiedMessage() {
+  copied.style.visibility = "visible";
+  setTimeout(() => {
+    copied.style.visibility = "hidden";
+  }, 3000);
+}
